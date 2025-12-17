00eb7cdc01c08535d10151bc6aa1c8f4159c71d1
Your Name <you@example.com>
2025-12-17 18:30:35 +0100

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
-""" 2️⃣ Typical responsibilities inside data_ingestion.py
-✅ 1. Define data sources
+"""
+1. Typical responsibilities inside data_ingestion.py
+2.  Define data sources
 
 Where the data comes from:
 
@@ -49,5 +50,25 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
         
+    def split_data(self, df: pd.DataFrame) -> tuple:
+        try:
+            logging.info("splitting data into train and test sets")
+            train_data_path = self.config.train_data_path
+            test_data_path = self.config.test_data_path
 
+            os.makedirs(os.path.dirname(train_data_path), exist_ok=True)
+            os.makedirs(os.path.dirname(test_data_path), exist_ok=True)
+
+            train_set, test_set = train_test_split(df, test_size= self.config.test_size, random_state=42)
+            train_set.to_csv(train_data_path, index= False)
+            test_set.to_csv(test_data_path, index= False)
+            logging.info(f"train and test data saved at: {train_data_path} and {test_data_path}")
+            return train_set, test_set
+        except Exception as e:
+            raise CustomException(e, sys)
         
+if __name__ == "__main__":
+    config_path = "src/config/config.yaml"
+    data_ingestion = DataIngestion(config_path)
+    df = data_ingestion.initiate_data_ingestion()
+    train_set, test_set = data_ingestion.split_data(df)
